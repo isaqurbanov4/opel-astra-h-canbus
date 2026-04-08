d4d3aa1341d2a055c67b1a53f9169bda27362f96
isaqurbanov4 <isaqurbanov4@gmail.com>
2026-04-08 18:42:18 +0000

    Kod strukturu kömək üçün hazırlandı

--- esp32_can_reader.ino
+++ esp32_can_reader.ino
@@ -1,32 +1,41 @@
 cat <<EOF > esp32_can_reader.ino
-#include <SPI.h>
 #include <mcp_can.h>
+#include <SPI.h>
 
-const int SPI_CS_PIN = 5;
-MCP_CAN CAN(SPI_CS_PIN);
+long unsigned int rxId;
+unsigned char len = 0;
+unsigned char rxBuf[8];
+MCP_CAN CAN0(5); // ESP32-də CS pini adətən 5 olur
 
 void setup() {
-    Serial.begin(115200);
-    if(CAN_OK == CAN.begin(CAN_500KBPS)) {
-        Serial.println("MCP2515 Hazirdir!");
-    } else {
-        Serial.println("Xeta: MCP2515 tapilmadi!");
-    }
+  Serial.begin(115200);
+  if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_8MHz) == CAN_OK) 
+    Serial.println("MCP2515 Hazirdir!");
+  else 
+    Serial.println("Xeta: MCP2515 tapilmadi...");
+  
+  CAN0.setMode(MCP_NORMAL);
 }
 
 void loop() {
-    unsigned char len = 0;
-    unsigned char buf[8];
-    if(CAN_MSGAVAIL == CAN.checkReceive()) {
-        CAN.readMsgBuf(&len, buf);
-        Serial.print("ID: 0x");
-        Serial.print(CAN.getCanId(), HEX);
-        Serial.print(" DATA: ");
-        for(int i = 0; i<len; i++) {
-            Serial.print(buf[i], HEX);
-            Serial.print(" ");
-        }
-        Serial.println();
+  if(!digitalRead(4)) { // İnt (kəsilmə) pini 4-cü pin
+    CAN0.readMsgBuf(&rxId, &len, rxBuf);
+    
+    // --- KÖMƏK EDƏCƏK ŞƏXSLƏR BURAYA ID ƏLAVƏ EDƏ BİLƏR ---
+    
+    if(rxId == 0x108) {
+       Serial.print("Motor Dövriyyəsi (Ehtimal): ");
+       Serial.println(rxBuf[0]); // Nümunə analiz
     }
+    
+    // Digər ID-ləri bura əlavə edəcəyik...
+    
+    Serial.print("ID: "); Serial.print(rxId, HEX);
+    Serial.print(" Data: ");
+    for(int i = 0; i<len; i++) {
+      Serial.print(rxBuf[i], HEX); Serial.print(" ");
+    }
+    Serial.println();
+  }
 }
 EOF
